676e7a1f8a6073e2012c72c1cb06ef29f8c0053d
guhcosta01 <110315816+guhcosta01@users.noreply.github.com>
2022-12-20 15:49:30 -0300

    Exerceio 1 finalizado

--- exercio/script.js
+++ exercio/script.js
@@ -2,15 +2,21 @@ function carregar(){
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('foto')
 var agora = new Date()
-var hora = agora.getHours() 
-msg.innerHTML = `Agora São ${hora} Horas `
+var hora = agora.getHours()
 
-if(hora > 6 && hora < 12 ) {
-    img.src = 'denoite.png'
-}
-else if(hora >= 12 && hora < 18){
-    img.src = "detarde.png"
+msg.innerHTML = `Agora São ${hora} Horas `
+if(hora >= 0 && hora < 12 ){
+    //bom dia
+    img.src = 'fotomanha.png'
+    document.body.style.background = "#9da1ac"
+   
+}else if (hora >= 12 && hora < 18){
+    // boa tarde
+    img.src = 'fototarde.jpg'
+    document.body.style.background = "rgb(253, 145, 63)"
 }else{
-    img.src = 'demanha.png'
+    //boa noite
+    img.src = 'fotonoite.jpg'
+    document.body.style.background = "#182325"
 }
 }
